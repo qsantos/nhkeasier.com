d68882b5d4712cc2ca23f3fe5af446a473b8a96a
Quentin Santos <quentin.santos@worldonline.fr>
2018-08-10 16:06:34 +0200

    Debug toolbar

--- nhkeasier/urls.py
+++ nhkeasier/urls.py
@@ -33,3 +33,8 @@ handler500 = nhkstories.views.handler500
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
