ae426ce3f986aa697349ff1610e37025ff7d278a
Quentin Santos <quentin.santos@worldonline.fr>
2018-09-23 12:16:34 +0200

    Always terminate URL patterns with '$'

--- nhkstories/urls.py
+++ nhkstories/urls.py
@@ -5,11 +5,11 @@ from . import feed
 
 app_name = 'nhkstories'
 urlpatterns = [
-    url(r'^story/(?P<id>[0-9]*)/', views.story, name='story'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^feed/', feed.LatestStoriesFeed(), name='feed'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/', views.archive, name='archive'),
-    url(r'^external-error/(?P<code>\d{3})/', views.external_error, name='external_error'),
+    url(r'^story/(?P<id>[0-9]*)/$', views.story, name='story'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^feed/$', feed.LatestStoriesFeed(), name='feed'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', views.archive, name='archive'),
+    url(r'^external-error/(?P<code>\d{3})/$', views.external_error, name='external_error'),
     url(r'^400/$', views.handler400),
     url(r'^403/$', views.handler403),
     url(r'^404/$', views.handler404),
