7fb22aabd0d77e46a179a771730b63f71750d451
Quentin Santos <quentin.santos@worldonline.fr>
2019-02-07 21:24:42 +0100

    Crawl /r/NHKEasyNews posts

---
+++ nhkstories/management/commands/update-reddit-posts.py
@@ -0,0 +1,106 @@
+import re
+import json
+import logging
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+from django.core.management.base import BaseCommand
+
+from nhkstories.logging import init_logging
+from nhkstories.models import Story
+
+logger = logging.getLogger(__name__)
+
+STORY_URL_PATTERN = r'https?://www3.nhk.or.jp/news/easy/(k[0-9]*)/k[0-9]*.html'
+SUBREDDIT_NEW_URL = 'https://www.reddit.com/r/NHKEasyNews/new.json'
+
+
+def fetch(url):
+    request = Request(url, headers={'User-Agent': 'NHKEasier Crawler'})
+    with urlopen(request) as f:
+        return f.read()
+
+
+def submissions_from_pushshift():
+    base = 'https://api.pushshift.io/reddit/search/submission/'
+    params = {
+        'subreddit': 'NHKEasyNews',
+        'sort': 'asc',
+        'sort_type': 'created_utc',
+        'size': 1000,
+    }
+    while True:
+        result = fetch(base + '?' + urlencode(params)).decode()
+        submissions = json.loads(result)['data']
+        for submission in submissions:
+            yield submission
+        if len(submissions) < 1000:
+            break
+        params['after'] = submissions[-1]['created_utc']
+
+
+def submissions_from_reddit():
+    result = fetch(SUBREDDIT_NEW_URL).decode()
+    result = json.loads(result)
+    assert result['kind'] == 'Listing'
+    for thing in result['data']['children']:
+        assert thing['kind'] == 't3'
+        data = thing['data']
+        data['full_link'] = data['url']
+        yield data
+
+
+def stories_with_urls(submissions):
+    for submission in submissions:
+        try:
+            selftext = submission['selftext']
+        except KeyError:
+            continue
+
+        m = re.search(STORY_URL_PATTERN, selftext)
+        if m is None:
+            # no case where story_id could be retrieved from URL
+            continue
+        story_id = m.group(1)
+
+        yield story_id, submission['full_link']
+
+
+def main(archive):
+    stories_by_story_id = {
+        story.story_id: story
+        for story in Story.objects.all()
+    }
+
+    if archive:
+        submissions = submissions_from_pushshift()
+    else:
+        submissions = submissions_from_reddit()
+    for story_id, url in stories_with_urls(submissions):
+        try:
+            story = stories_by_story_id[story_id]
+        except KeyError:
+            continue
+
+        if story.r_nhkeasynews_link is not None:
+            continue
+
+        print(story_id)
+        story.r_nhkeasynews_link = url
+        story.save()
+
+
+class Command(BaseCommand):
+    help = 'Search NHKEasyNews posts'
+
+    def add_arguments(self, parser):
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('--new', action='store_true')
+        group.add_argument('--archive', action='store_true')
+
+    def handle(self, archive, *args, **kwargs):
+        init_logging()
+        try:
+            main(archive)
+        except Exception:
+            logger.exception('NHKUPDATE GENERAL FAILURE')
