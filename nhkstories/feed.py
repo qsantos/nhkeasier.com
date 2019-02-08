4d173a4eb4fc6df71b04dae3f4e8c46ed6ffb537
Quentin Santos <quentin.santos@worldonline.fr>
2019-02-08 07:19:18 +0100

    Update story links in feed

--- nhkstories/feed.py
+++ nhkstories/feed.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
 
@@ -20,6 +22,8 @@ class LatestStoriesFeed(Feed):
 
     def item_description(self, story):
         html = ''
+
+        # content
         if story.video_reencoded and story.image:
             html += '<video src="{}" poster="{}" controls preload="none""></video>'.format(story.video_reencoded.url, story.image.url)
         elif story.video_reencoded:
@@ -28,8 +32,17 @@ class LatestStoriesFeed(Feed):
             html += '<img src="{}" alt="Story illustration">'.format(story.image.url)
         html += story.content_with_ruby
         if story.voice:
-            html += '<audio src={} controls preload="none"></audio>'.format(story.voice.url)
-        html += '<a href="https://www3.nhk.or.jp/news/easy/{0}/{0}.html">See on NHK News Web Easy</a> (if still available)'.format(story.story_id)
+            html += '<audio src="{}" controls preload="none"></audio>'.format(story.voice.url)
+
+        # links
+        html += '<ul>'
+        if story.r_nhkeasynews_link:
+            html += '<li><a href="{}">/r/NHKEasyNews</a></li>'.format(story.r_nhkeasynews_link)
+        if story.published.date() >= date(2017, 12, 5):
+            html += '<li><a href="https://www3.nhk.or.jp/news/easy/{0}/{0}.html">Original</a></li>'.format(story.story_id)
+        html += '<li><a href="{}" class="permalink">Permalink</a></li>'.format(reverse('nhkstories:story', args=[story.id]))
+        html += '</ul>'
+
         return html
 
     def item_link(self, item):
