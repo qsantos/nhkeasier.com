1cd9dcdc56541a6e5c956cc033d83e609a076ff0
Quentin Santos <qsantos@qsantos.fr>
2023-12-19 20:04:40 +0100

    Copy description of deinflect.dat to deinflect.rs

--- edict2/src/deinflect.rs
+++ edict2/src/deinflect.rs
@@ -15,6 +15,26 @@ pub struct Candidate {
     pub type_: u32,
 }
 
+// deinflect.dat countains instructions to remove inflections from words
+// the first line is a header
+// the next few lines (without '\t') are a string array refereced to later
+// the rest are made of four fields separated by '\t'
+//   * first field incdicates the suffix to look for in a candidate
+//   * second field indicates when the suffix should be remplaced with
+//   * third field helps narrowing down the grammatical class of the candidate
+//   * fourth field points to the array string and gives a user-friendly
+//     explanation of the removed suffix
+// type is a bit field where:
+//   * bit 0 hints at a 一段 verb ('v1' marker)
+//   * bit 1 hints at a 五段 verb (markers starting with 'v5')
+//   * bit 2 hints at a い-adjective (marker 'adj-i')
+//   * bit 3 hints at a くる verb (marker 'vk')
+//   * bit 4 hints at a す or する verb (markers starting with 'vs-')
+//   * bit 7 should always be set for words (so that 0xff & wtype != 0 always)
+// for a word, type gives a hint of the expected grammatical class of the word
+// for a rule, type[0:8] gives the required grammatical class of original word
+// for a rule, type[8:16] gives the grammatical class of the resulting word
+// thus, the new word has type wtype = rtyle >> 8
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Deinflector<'a> {
     rules: HashMap<&'a str, Vec<Rule>>,
