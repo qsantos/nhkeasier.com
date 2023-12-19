3ed53c045c07ef27cf9dbc4702d072a841a90d32
Quentin Santos <qsantos@qsantos.fr>
2023-12-19 18:13:39 +0100

    Use buffered output

--- edict2/src/main.rs
+++ edict2/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{BufWriter, Write, stdout};
+
 use edict2::{Deinflector, Edict, iter_fragments};
 
 fn main() {
@@ -8,12 +10,14 @@ fn main() {
     let deinflector = Deinflector::parse(&data);
 
     let data = std::fs::read_to_string("test-input").unwrap();
+    let mut writer = BufWriter::with_capacity(8192,stdout().lock());
     for fragment in iter_fragments(&data) {
         for candidate in deinflector.deinflect(fragment) {
             if let Some(entries) = edict2.lookup(&candidate.word as &str) {
                 for entry in entries {
                     if entry.type_ & candidate.type_ != 0 {
-                        println!("{}", entry.line);
+                        let _ = writer.write(entry.line.as_bytes()).unwrap();
+                        let _ = writer.write(b"\n").unwrap();
                     }
                 }
             }
