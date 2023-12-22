77bb1ac368b911feed95d66bbca26409c07a7988
Quentin Santos <qsantos@qsantos.fr>
2023-12-22 07:01:01 +0100

    Basic querying with sqlx

--- nhkeasier-rs/src/main.rs
+++ nhkeasier-rs/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::types::chrono::{DateTime, Utc};
+use sqlx::FromRow;
+
+#[derive(Debug, FromRow)]
+#[allow(dead_code)]
+struct Story {
+    id: u32,
+    story_id: String,
+    published: DateTime<Utc>,
+    title_with_ruby: String,
+    title: String,
+    content_with_ruby: String,
+    content: String,
+    webpage: String,
+    image: String,
+    voice: String,
+    video_original: String,
+    video_reencoded: String,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("db.sqlite3")
+        .await?;
+
+    let story = sqlx::query_as::<_, Story>("SELECT * FROM nhkeasier_story ORDER BY id DESC")
+        .fetch_one(&pool)
+        .await?;
+
+    println!("{story:?}");
+
+    Ok(())
 }
