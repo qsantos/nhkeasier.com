fbd76ea1015fb389db10b3a750a761fee24cdd80
Quentin Santos <qsantos@qsantos.fr>
2023-12-23 11:36:47 +0100

    Use zero-copy in sqlx query and template

--- nhkeasier-rs/src/main.rs
+++ nhkeasier-rs/src/main.rs
@@ -5,38 +5,38 @@ use sqlx::FromRow;
 
 use edict2::{SubEdictCreator, SubEnamdictCreator};
 
-#[derive(Debug, FromRow)]
+#[derive(Clone, Debug, FromRow)]
 #[allow(dead_code)]
-struct Story {
+struct Story<'a> {
     id: i64,
-    story_id: String,
+    story_id: &'a str,
     published: NaiveDateTime,
-    title_with_ruby: String,
-    title: String,
-    content_with_ruby: Option<String>,
-    content: Option<String>,
-    image: Option<String>,
-    voice: Option<String>,
-    video_original: Option<String>,
-    video_reencoded: Option<String>,
+    title_with_ruby: &'a str,
+    title: &'a str,
+    content_with_ruby: Option<&'a str>,
+    content: Option<&'a str>,
+    image: Option<&'a str>,
+    voice: Option<&'a str>,
+    video_original: Option<&'a str>,
+    video_reencoded: Option<&'a str>,
     subedict_created: bool,
-    webpage: Option<String>,
+    webpage: Option<&'a str>,
 }
 
 #[derive(Template)]
 #[template(path = "story.html")]
-struct MyTemplate {
+struct MyTemplate<'a> {
     debug: bool,
-    title: String,
-    description: Option<String>,
-    image: Option<String>,
-    player: Option<String>,
-    header: String,
-    story: Story,
+    title: &'a str,
+    description: Option<&'a str>,
+    image: Option<&'a str>,
+    player: Option<&'a str>,
+    header: &'a str,
+    story: &'a Story<'a>,
     previous_story_id: Option<i64>,
     next_story_id: Option<i64>,
-    edict: Option<String>,
-    enamdict: Option<String>,
+    edict: Option<&'a str>,
+    enamdict: Option<&'a str>,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -49,30 +49,36 @@ async fn main() -> Result<(), sqlx::Error> {
     let sub_edict_creator = SubEdictCreator::from_files();
     let sub_enamdict_creator = SubEnamdictCreator::from_files();
 
-    let story = sqlx::query_as!(Story, "SELECT * FROM nhkeasier_story ORDER BY id DESC")
+    let row = sqlx::query("SELECT * FROM nhkeasier_story ORDER BY id DESC")
         .fetch_one(&pool)
         .await?;
+    let story = Story::from_row(&row)?;
 
+    let edict = story
+        .content
+        .as_ref()
+        .map(|content| sub_edict_creator.from(content).join("\n"));
+    let enamdict = story
+        .content
+        .as_ref()
+        .map(|content| sub_enamdict_creator.from(content).join("\n"));
     let t = MyTemplate {
         debug: true,
-        title: story.title.clone(),
-        description: story.content.clone(),
-        image: story.image.clone(),
+        title: story.title,
+        description: story.content,
+        image: story.image,
         player: None,
-        header: "Single Story".to_string(),
+        header: "Single Story",
         previous_story_id: Some(story.id - 1),
         next_story_id: None,
-        edict: story
-            .content
-            .as_ref()
-            .map(|content| sub_edict_creator.from(content).join("\n")),
-        enamdict: story
-            .content
-            .as_ref()
-            .map(|content| sub_enamdict_creator.from(content).join("\n")),
-        story,
-    };
-    println!("{}", t.render().unwrap());
+        edict: edict.as_deref(),
+        enamdict: enamdict.as_deref(),
+        story: &story,
+    }
+    .render()
+    .unwrap();
+
+    println!("{t}");
 
     Ok(())
 }
