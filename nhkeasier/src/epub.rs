7e7db98d0bf5efcf81731eb13f4632a8c95b9cc6
Quentin Santos <qsantos@qsantos.fr>
2024-01-06 09:29:34 +0100

    epub: deduplicate Story/connect_to_database/JST

--- nhkeasier/src/epub.rs
+++ nhkeasier/src/epub.rs
@@ -2,46 +2,12 @@ use std::fs::File;
 use std::io::Write;
 
 use askama::Template;
-use chrono::{FixedOffset, NaiveDateTime, TimeZone, Utc};
+use chrono::{TimeZone, Utc};
 use regex::Regex;
-use sqlx::sqlite::SqlitePoolOptions;
-use sqlx::{FromRow, Pool, Sqlite};
+use sqlx::FromRow;
 use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
-// TODO: deduplicate
-#[derive(Clone, Debug, FromRow)]
-#[allow(dead_code)]
-pub struct Story<'a> {
-    pub id: i64,
-    pub story_id: &'a str,
-    pub published: NaiveDateTime,
-    pub title_with_ruby: &'a str,
-    pub title: &'a str,
-    pub content_with_ruby: Option<&'a str>,
-    pub content: Option<&'a str>,
-    pub image: Option<&'a str>,
-    pub voice: Option<&'a str>,
-    pub video_original: Option<&'a str>,
-    pub video_reencoded: Option<&'a str>,
-    pub subedict_created: bool,
-    pub webpage: Option<&'a str>,
-}
-
-// TODO: deduplicate
-pub async fn connect_to_database() -> Pool<Sqlite> {
-    let database_url =
-        std::env::var("DATABASE_URL").expect("missing environment variable DATABASE_URL");
-    SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url)
-        .await
-        .expect("failed to connect to database")
-}
-
-// TODO: deduplicate
-lazy_static::lazy_static! {
-    pub static ref JST: FixedOffset = FixedOffset::east_opt(3600 * 9).unwrap();
-}
+use crate::{connect_to_database, Story, JST};
 
 #[derive(Template)]
 #[template(path = "epub/EPUB/content.opf", escape = "xml")]
